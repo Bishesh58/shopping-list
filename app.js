252ea8fd622edeb7eb9553500c0b4680b6646f3c
Bishesh Sunam <bishesh@Bishesh.local>
2020-08-20 23:30:13 +1200

    replaced app.js file

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
+var input = document.getElementById("userInput");
 var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var li = document.getElementsByTagName('li');
 
-function inputLength() {
-	return input.value.length;
+
+function inputLength (){
+   return input.value.length;
 }
 
-function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+function captureInput(event){
+    if(event.keyCode === 13 && inputLength() > 0){
+        addList();
+    }
 }
 
-function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+input.addEventListener('keypress', captureInput);
+
+function buttonAdd(){
+    if(inputLength() > 0){
+        addList();
+    }
 }
 
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+button.addEventListener('click', buttonAdd);
+
+function addList(){
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
 }
 
-button.addEventListener("click", addListAfterClick);
 
-input.addEventListener("keypress", addListAfterKeypress);
